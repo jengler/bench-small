89a6f72ac042017643a4e2d8facdded87e2ad9cd
jengler <jengler>
2024-01-22 22:27:01 +0000

    remove old files

--- nb_one/nb_one_3.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-74e5fc82-a988-11ed-aafc-acde48001122 74e5fdcc-a988-11ed-aafc-acde48001122
-74e5fe12-a988-11ed-aafc-acde48001122 74e5fe3a-a988-11ed-aafc-acde48001122
-74e5fe62-a988-11ed-aafc-acde48001122 74e5fe8a-a988-11ed-aafc-acde48001122
-74e5feb2-a988-11ed-aafc-acde48001122 74e5fed0-a988-11ed-aafc-acde48001122
-74e5fef8-a988-11ed-aafc-acde48001122 74e5ff20-a988-11ed-aafc-acde48001122
-74e5ff48-a988-11ed-aafc-acde48001122 74e5ff66-a988-11ed-aafc-acde48001122
-74e5ff8e-a988-11ed-aafc-acde48001122 74e5ffac-a988-11ed-aafc-acde48001122
--- notebooks/Users/jengler@databricks.com/Test Cases/Legacy Git Integation Test Case.py
+++
@@ -1,6 +0,0 @@
-# Databricks notebook source
-# Setup this test case by using the legacy git integration.
-
-# Adding one plus two
-result = 1 + 2
-print(result)
